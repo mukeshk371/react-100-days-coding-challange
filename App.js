0cfc4fbaebac0d24a664c88d33ee193cb5d68649
mukeshk371 <mukeshk371@gmail.com>
2024-01-25 21:01:28 +0530

    Day 1 : Nested elements created by using React.createElement()  and added to the root

--- App.js
+++ App.js
@@ -7,16 +7,37 @@ const heading = React.createElement(
 
 // Nested elements
 
-const parent = React.createElement("div",{ id: "parent" },
-    React.createElement("div",{ id: "child" },
+const parent = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" },
         [
-            React.createElement("h1",{},"Header 1"),
-            React.createElement("h2",{},"Header 2")
+            React.createElement("h1", {}, "Header 1"),
+            React.createElement("h2", {}, "Header 2")
         ]
     )
 );
 
+// Complex Nested elements
+
+const complexNestedElement = React.createElement("div", { className: "row" },
+    [
+        React.createElement("section", { className: "col-12 header" },
+            React.createElement("h1", {}, "Header")
+        ),
+        React.createElement("section", { className: "col-6 left-section" },
+            React.createElement("h2", {}, "Left Section")
+        ),
+        React.createElement("section", { class: "col-6 right-section" },
+            React.createElement("h2", {}, "Right Section")
+        ),
+        React.createElement("section", { className: "col-12 footer" },
+            React.createElement("h1", {}, "Footer")
+        )
+    ]
+);
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(parent); // This code will not apply
+root.render(complexNestedElement); // This code will apply
 console.log(parent);
