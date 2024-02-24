b2beee856c800ef4d07d7a6ec5998f469ee5b2c2
mukeshk371 <mukeshk371@gmail.com>
2024-02-24 23:18:02 +0530

    Day 31: Food Delivery App - Login Functionality added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,29 @@ import { HeaderStyles } from "../styles/HeaderStyles";
 
 export const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [loginName, setLoginName] = useState("Login");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
+  const [showLoginPopup, setShowLoginPopup] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const loginBtn = () => {
-    loginName == "Mukesh Kumar" ? setLoginName("Login") : setLoginName("Mukesh Kumar");
-  }
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setIsLoggedIn(true);
+    setUsername(e.target.username.value);
+    setShowLoginPopup(false);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setUsername("");
+  };
+
+  const toggleLoginPopup = () => {
+    setShowLoginPopup(!showLoginPopup);
+  };
 
   return (
     <HeaderStyles>
@@ -52,11 +66,11 @@ export const Header = () => {
               </a>
             </li>
             <li className="flex items-center md:ml-[60px]">
-              <a className="flex items-center h-full px-[25px] py-[10px] md:px-0 md:py-0" href="#!" onClick={loginBtn}>
+              <a className="flex items-center h-full px-[25px] py-[10px] md:px-0 md:py-0" href="#!" onClick={isLoggedIn ? handleLogout : toggleLoginPopup}>
                 <svg viewBox="6 0 12 24" height="19" width="18" fill="#686b78">
                   <path d="M11.9923172,11.2463768 C8.81761115,11.2463768 6.24400341,8.72878961 6.24400341,5.62318841 C6.24400341,2.5175872 8.81761115,0 11.9923172,0 C15.1670232,0 17.740631,2.5175872 17.740631,5.62318841 C17.740631,8.72878961 15.1670232,11.2463768 11.9923172,11.2463768 Z M11.9923172,9.27536232 C14.0542397,9.27536232 15.7257581,7.64022836 15.7257581,5.62318841 C15.7257581,3.60614845 14.0542397,1.97101449 11.9923172,1.97101449 C9.93039471,1.97101449 8.25887628,3.60614845 8.25887628,5.62318841 C8.25887628,7.64022836 9.93039471,9.27536232 11.9923172,9.27536232 Z M24,24 L0,24 L1.21786143,19.7101449 L2.38352552,15.6939891 C2.85911209,14.0398226 4.59284263,12.7536232 6.3530098,12.7536232 L17.6316246,12.7536232 C19.3874139,12.7536232 21.1256928,14.0404157 21.6011089,15.6939891 L22.9903494,20.5259906 C23.0204168,20.63057 23.0450458,20.7352884 23.0641579,20.8398867 L24,24 Z M21.1127477,21.3339312 L21.0851024,21.2122487 C21.0772161,21.1630075 21.0658093,21.1120821 21.0507301,21.0596341 L19.6614896,16.2276325 C19.4305871,15.4245164 18.4851476,14.7246377 17.6316246,14.7246377 L6.3530098,14.7246377 C5.4959645,14.7246377 4.55444948,15.4231177 4.32314478,16.2276325 L2.75521062,21.6811594 L2.65068631,22.0289855 L21.3185825,22.0289855 L21.1127477,21.3339312 Z"></path>
                 </svg>
-                <span className="truncate">{loginName}</span>
+                <span className="truncate capitalize">{isLoggedIn ? username : "Login"}</span>
               </a>
             </li>
             <li className="flex items-center md:ml-[60px]">
@@ -96,6 +110,29 @@ export const Header = () => {
           </button>
         </div>
       </header>
+
+      {!isLoggedIn && showLoginPopup && (
+        <div className="login-popup fixed h-full w-full bg-[rgba(0,0,0,0.6)] top-0 left-0 z-[999]">
+          <div className="login-form mx-auto bg-white w-[50%] relative rounded-[10px] mt-[150px]">
+            <form onSubmit={handleLogin} className="flex flex-col justify-center items-center p-[20px]">
+              <label className="flex justify-center items-center w-[70%] mb-[20px]">
+                <strong className="font-semibold text-[20px] mr-[20px]">Username:</strong>
+                <input type="text" name="username" required className="search-input border-[rgba(40,44,63,.2)] border-[1px] rounded-[4px] h-[48] w-full px-[16px]" />
+              </label>
+              <label className="flex justify-center items-center w-[70%] mb-[20px]">
+                <strong className="font-semibold text-[20px] mr-[20px]">Password:</strong>
+                <input type="password" name="password" required className="search-input border-[rgba(40,44,63,.2)] border-[1px] rounded-[4px] h-[48] w-full px-[16px]" />
+              </label>
+              <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login</button>
+            </form>
+            <button className="absolute right-[15px] top-[15px] text-[red]" onClick={() => setShowLoginPopup(false)}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      )}
     </HeaderStyles>
   );
 };
